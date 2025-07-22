019b32effcea6ce73fdca15754cd26e92126b13a
fan <enbofan663@gmail.com>
2025-07-23 06:31:00 +0800

    修改syncer

--- src/v1/syncer/syncer.cpp
+++ src/v1/syncer/syncer.cpp
@@ -1,36 +1,39 @@
 
 #include "syncer.hpp"
 #include "update_package.hpp"
-#include <chrono>
 
 namespace world_exe::v1::sync {
 class Syncer::Impl {
 public:
-    Impl() { sync_data_buffer_.reserve(50); }
-
-    std::tuple<const interfaces::IPreDictorUpdatePackage&, bool> await(double t_second = 2) {
-        bool ready { false };
-        const auto end_time_point =
-            std::chrono::steady_clock::now() + std::chrono::duration<double>(t_second);
-        while (std::chrono::steady_clock::now() <= end_time_point) {
-            std::lock_guard<std::mutex> lock(mutex_);
-            const auto data_num = sync_data_buffer_.size();
-            for (size_t i = data_num - 1; i >= 0; i--) {
-                if (std::abs((camera_capture_end_time_stamp_
-                                 - sync_data_buffer_[i].camera_capture_begin_time_stamp)
-                        - delay_)
-                    <= threshold) {
-                    ready = true;
-                    predict_update_package_.SetTimeStamp(
-                        sync_data_buffer_[i].camera_capture_begin_time_stamp);
-                    predict_update_package_.SetTransform(sync_data_buffer_[i].camera_to_gimbal);
-                }
-            }
-            sync_data_buffer_.clear();
-            if (ready) break;
+    Impl() {
+        // sync_data_buffer_.reserve(50);
         }
 
-        return { predict_update_package_, ready };
+    std::tuple<const interfaces::IPreDictorUpdatePackage&, bool> await(double t_second = 2) {
+        // bool ready { false };
+        // const auto end_time_point =
+        //     std::chrono::steady_clock::now() + std::chrono::duration<double>(t_second);
+        // while (std::chrono::steady_clock::now() <= end_time_point) {
+        //     std::lock_guard<std::mutex> lock(mutex_);
+        //     const auto data_num = sync_data_buffer_.size();
+        //     for (std::size_t i = data_num; i-- > 0;) {
+        //         if (std::abs((camera_capture_end_time_stamp_
+        //                          - sync_data_buffer_[i].camera_capture_begin_time_stamp)
+        //                 - delay_)
+        //             <= threshold) {
+        //             ready = true;
+        //             predict_update_package_.SetTimeStamp(
+        //                 sync_data_buffer_[i].camera_capture_begin_time_stamp);
+        //             predict_update_package_.SetTransform(sync_data_buffer_[i].camera_to_gimbal);
+        //         }
+        //     }
+        //     sync_data_buffer_.clear();
+        //     if (ready) break;
+        // }
+
+        predict_update_package_.SetTimeStamp(sync_data_.camera_capture_begin_time_stamp);
+        predict_update_package_.SetTransform(sync_data_.camera_to_gimbal);
+        return { predict_update_package_, true };
     }
 
     void setTimeStamp(const std::time_t& time) { camera_capture_end_time_stamp_ = time; }
@@ -41,7 +44,8 @@ public:
 
     void LoadCallback(const data::CameraGimbalMuzzleSyncData& data) {
         std::lock_guard<std::mutex> lock(mutex_);
-        sync_data_buffer_.emplace_back(data);
+        // sync_data_buffer_.emplace_back(data);
+        sync_data_= data;
     }
 
 private:
@@ -52,7 +56,8 @@ private:
     static constexpr std::time_t threshold = 1e6;
 
     std::mutex mutex_;
-    std::vector<data::CameraGimbalMuzzleSyncData> sync_data_buffer_;
+    data::CameraGimbalMuzzleSyncData sync_data_;
+    // std::vector<data::CameraGimbalMuzzleSyncData> sync_data_buffer_;
 };
 
 Syncer::Syncer()
