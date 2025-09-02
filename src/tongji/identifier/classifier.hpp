477a35fd443bfc7894cc51baddc1ee92e79b1cab
heyeuu <2829004293@qq.com>
2025-09-02 19:42:37 +0800

    fix(classifier): resolve issues in classifier class implementation

--- src/tongji/identifier/classifier.hpp
+++ src/tongji/identifier/classifier.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <memory>
+#include <opencv2/core/mat.hpp>
+
+#include "enum/armor_id.hpp"
 namespace world_exe::tongji::identifier {
 
 class Classifier final {
 public:
-    Classifier();
+    Classifier(const std::string& model_path, int model_image_width, int model_image_height);
     ~Classifier();
+
+    enumeration::ArmorIdFlag classify(const cv::Mat& armor_pattern);
+
+    enumeration::ArmorIdFlag ovclassify(const cv::Mat& armor_pattern);
 
 private:
     class Impl;
