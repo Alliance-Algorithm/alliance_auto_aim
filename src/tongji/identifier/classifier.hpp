1586caf13035921a649b4ac1c1bfa9f41028dba9
heyeuu <2829004293@qq.com>
2025-09-04 04:24:10 +0800

    feat(identifier): add identifier module for armor identification

--- src/tongji/identifier/classifier.hpp
+++ src/tongji/identifier/classifier.hpp
@@ -1,23 +1,153 @@
 #pragma once
 
-#include <memory>
 #include <opencv2/core/mat.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <openvino/runtime/core.hpp>
 
 #include "enum/armor_id.hpp"
-namespace world_exe::tongji::identifier {
 
+namespace world_exe::tongji::identifier {
 class Classifier final {
 public:
-    Classifier(const std::string& model_path, int model_image_width, int model_image_height);
-    ~Classifier();
+    explicit Classifier(
+        const std::string& model_path, int model_image_width, int model_image_height)
+        : model_image_width_(model_image_width)
+        , model_image_height_(model_image_height) {
+        net_            = cv::dnn::readNetFromONNX(model_path);
+        auto ovmodel    = core_.read_model(model_path);
+        compiled_model_ = core_.compile_model(
+            ovmodel, "AUTO", ov::hint::performance_mode(ov::hint::PerformanceMode::LATENCY));
+    }
 
-    enumeration::ArmorIdFlag classify(const cv::Mat& armor_pattern);
+    void Classify(const cv::Mat& armor_pattern, enumeration::ArmorIdFlag& armor_id,
+        double& armor_confidence) {
+        if (armor_pattern.empty()) {
+            armor_id = enumeration::ArmorIdFlag::Unknow;
+        }
 
-    enumeration::ArmorIdFlag ovclassify(const cv::Mat& armor_pattern);
+        cv::Mat gray;
+        cv::cvtColor(armor_pattern, gray, cv::COLOR_BGR2GRAY);
+
+        auto input   = cv::Mat(model_image_height_, model_image_width_, CV_8UC1, cv::Scalar(0));
+        auto x_scale = static_cast<double>(model_image_width_) / gray.cols;
+        auto y_scale = static_cast<double>(model_image_height_) / gray.rows;
+        auto scale   = std::min(x_scale, y_scale);
+        auto h       = static_cast<int>(gray.rows * scale);
+        auto w       = static_cast<int>(gray.cols * scale);
+
+        if (h == 0 || w == 0) {
+            armor_id = enumeration::ArmorIdFlag::Unknow;
+        }
+        auto roi = cv::Rect(0, 0, w, h);
+        cv::resize(gray, input(roi), { w, h });
+
+        auto blob = cv::dnn::blobFromImage(input, 1.0 / 255.0, cv::Size(), cv::Scalar());
+
+        net_.setInput(blob);
+        cv::Mat outputs = net_.forward();
+
+        // softmax
+        float max = *std::max_element(outputs.begin<float>(), outputs.end<float>());
+        cv::exp(outputs - max, outputs);
+        float sum = cv::sum(outputs)[0];
+        outputs /= sum;
+
+        cv::Point label_point;
+        cv::minMaxLoc(outputs.reshape(1, 1), nullptr, &armor_confidence, nullptr, &label_point);
+        int label_id = label_point.x;
+
+        if (label_id == 3) {
+            armor_id = enumeration::ArmorIdFlag::InfantryIII;
+        } else if (label_id == 4) {
+            armor_id = enumeration::ArmorIdFlag::InfantryIV;
+        } else if (label_id == 5) {
+            armor_id = enumeration::ArmorIdFlag::InfantryV;
+        } else if (label_id == 6) {
+            armor_id = enumeration::ArmorIdFlag::Outpost;
+        } else if (label_id == 1) {
+            armor_id = enumeration::ArmorIdFlag::Hero;
+        } else if (label_id == 2) {
+            armor_id = enumeration::ArmorIdFlag::Engineer;
+        } else if (label_id == 0) {
+            armor_id = enumeration::ArmorIdFlag::Sentry;
+        } else if (label_id == 7) {
+            armor_id = enumeration::ArmorIdFlag::Base;
+        };
+    }
+
+    void OvClassify(const cv::Mat& armor_pattern, enumeration::ArmorIdFlag& armor_id,
+        double& armor_confidence) {
+        if (armor_pattern.empty()) {
+            armor_id = enumeration::ArmorIdFlag::Unknow;
+        }
+
+        cv::Mat gray;
+        cv::cvtColor(armor_pattern, gray, cv::COLOR_BGR2GRAY);
+
+        // Resize image
+        auto input   = cv::Mat(model_image_height_, model_image_width_, CV_8UC1, cv::Scalar(0));
+        auto x_scale = static_cast<double>(model_image_width_) / gray.cols;
+        auto y_scale = static_cast<double>(model_image_height_) / gray.rows;
+        auto scale   = std::min(x_scale, y_scale);
+        auto h       = static_cast<int>(gray.rows * scale);
+        auto w       = static_cast<int>(gray.cols * scale);
+
+        if (h == 0 || w == 0) {
+            armor_id = enumeration::ArmorIdFlag::Unknow;
+        }
+
+        auto roi = cv::Rect(0, 0, w, h);
+        cv::resize(gray, input(roi), { w, h });
+        // Normalize the input image to [0, 1] range
+        input.convertTo(input, CV_32F, 1.0 / 255.0);
+
+        ov::Tensor input_tensor(ov::element::f32, { 1, 1, 32, 32 }, input.data);
+
+        ov::InferRequest infer_request = compiled_model_.create_infer_request();
+        infer_request.set_input_tensor(input_tensor);
+        infer_request.infer();
+
+        auto output_tensor = infer_request.get_output_tensor();
+        auto output_shape  = output_tensor.get_shape();
+        cv::Mat outputs(1, 9, CV_32F, output_tensor.data());
+
+        // Softmax
+        float max = *std::max_element(outputs.begin<float>(), outputs.end<float>());
+        cv::exp(outputs - max, outputs);
+        float sum = cv::sum(outputs)[0];
+        outputs /= sum;
+
+        cv::Point label_point;
+        cv::minMaxLoc(outputs.reshape(1, 1), nullptr, &armor_confidence, nullptr, &label_point);
+        int label_id = label_point.x;
+
+        if (label_id == 3) {
+            armor_id = enumeration::ArmorIdFlag::InfantryIII;
+        } else if (label_id == 4) {
+            armor_id = enumeration::ArmorIdFlag::InfantryIV;
+        } else if (label_id == 5) {
+            armor_id = enumeration::ArmorIdFlag::InfantryV;
+        } else if (label_id == 6) {
+            armor_id = enumeration::ArmorIdFlag::Outpost;
+        } else if (label_id == 1) {
+            armor_id = enumeration::ArmorIdFlag::Hero;
+        } else if (label_id == 2) {
+            armor_id = enumeration::ArmorIdFlag::Engineer;
+        } else if (label_id == 0) {
+            armor_id = enumeration::ArmorIdFlag::Sentry;
+        } else if (label_id == 7) {
+            armor_id = enumeration::ArmorIdFlag::Base;
+        };
+    };
 
 private:
-    class Impl;
-    std::unique_ptr<Impl> pimpl_;
-};
+    cv::dnn::Net net_;
+    ov::Core core_;
+    ov::CompiledModel compiled_model_;
 
+    const int model_image_height_ = 640;
+    const int model_image_width_  = 640;
+};
 }
---
+++ src/tongji/identifier/identifier.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "interfaces/identifier.hpp"
+
+namespace world_exe::tongji::identifier {
+
+enum Color { red, blue, extinguish, purple };
+
+struct Lightbar {
+    std::size_t id;
+    Color color;
+    cv::Point2f center, top, bottom, top2bottom;
+    std::vector<cv::Point2f> points;
+    double angle, angle_error, length, width, ratio;
+    cv::RotatedRect rotated_rect;
+
+    Lightbar(const cv::RotatedRect& rotated_rect, std::size_t id)
+        : id(id)
+        , rotated_rect(rotated_rect) {
+        std::vector<cv::Point2f> corners(4);
+        rotated_rect.points(&corners[0]);
+        std::sort(corners.begin(), corners.end(),
+            [](const cv::Point2f& a, const cv::Point2f& b) { return a.y < b.y; });
+
+        this->center     = rotated_rect.center;
+        this->top        = (corners[0] + corners[1]) / 2.;
+        this->bottom     = (corners[2] + corners[3]) / 2.;
+        this->top2bottom = bottom - top;
+
+        this->points.emplace_back(top);
+        this->points.emplace_back(bottom);
+
+        this->width       = cv::norm(corners[0] - corners[1]);
+        this->angle       = std::atan2(top2bottom.y, top2bottom.x);
+        this->angle_error = std::abs(angle - M_PI / 2);
+        this->length      = cv::norm(top2bottom);
+        this->ratio       = length / width;
+    }
+};
+class Identifier final : public interfaces::IIdentifier {
+public:
+    Identifier(const std::string& model_path, int model_image_width, int model_image_height);
+    ~Identifier();
+
+    const std::tuple<const std::shared_ptr<interfaces::IArmorInImage>, enumeration::CarIDFlag>
+    identify(const cv::Mat& input_image) override;
+
+    void SetTargetColor(Color target_color);
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+};
+
+}
