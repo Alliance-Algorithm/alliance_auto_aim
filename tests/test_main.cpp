b16c7d4181f83f2e1a90bde636b3f800f7a0c671
HarryPotter1tech <huangziang_2024@qq.com>
2025-10-09 21:24:43 +0800

    修改pnp mock,

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,6 +1,6 @@
-#include "test_main.hpp"
-
-using namespace world_exe::alliance_auto_aim::tests;
+#include "Pnpsolver.cpp" 
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 using namespace world_exe::interfaces;
 using namespace world_exe::data;
 using namespace world_exe::enumeration;
@@ -8,18 +8,9 @@ using namespace world_exe::enumeration;
 using ::testing::Return;
 using ::testing::_;
 
-TEST(DataFlowTest, Datain_out)
+TEST_F(PnpsolverTest,AbilityTest)
 {
-    MockPnpSolver Mock_solver;
-    auto mock_armor_in_image = std::make_shared<MockArmorInImage>();
-    auto mock_armor_in_camera = std::make_shared<MockArmorInCamera>();
-    std::shared_ptr<IArmorInImage> armor_in_image = mock_armor_in_image;
-    std::shared_ptr<IArmorInCamera> armor_in_camera = mock_armor_in_camera;
-
-    EXPECT_CALL(Mock_solver,SolvePnp(armor_in_image)).WillOnce(Return(armor_in_camera));
-
-    auto result = Mock_solver.SolvePnp(armor_in_image);
-    EXPECT_EQ(result, armor_in_camera);
+    RunTest();
 }
 
 int main(int argc, char **argv) {
