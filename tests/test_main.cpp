9f74e72ec32362f1d8576bad423fc05c1ad29b9b
黄子昂 <huangziang_2024@qq.com>
2025-10-10 21:47:40 +0800

    [CI] 更泛用的Pnpsolver测试接口 (#13)
    * 在test/ 增加了pnpsolver mock,通过了编译，测试了接口输入输出
    
    * [CI]: 开启pr自动测试
    
    * [CI] 重命名测试行文
    
    * [CI]  修复ubuntu22.4.test.test.dockerfile
    
    * [Fix]: 修复抽象类detail::TimeStamped
    
    * [Fix] 修复CMake 依赖导出问题
    
    * 修改pnp mock,
    
    * 添加googletest作为子模块
    
    * [Test] 修改命名空间，修正测试类型依赖
    
    * 修改测试类，继承自testing::TestWithParam<IPnpSolver*>参数化测试，传入使用接口类，修改命名空间
    
    ---------
    
    Co-authored-by: alray <1780284652@qq.com>
    Co-authored-by: Alray Qiu <70491601+AlrayQiu@users.noreply.github.com>

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,18 +1,28 @@
 #include "Pnpsolver.cpp" 
+#include "v1/pnpsolver/armor_pnp_solver.hpp"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-using namespace world_exe::interfaces;
-using namespace world_exe::data;
-using namespace world_exe::enumeration;
+
+using namespace world_exe::v1::pnpsolver;
+using world_exe::parameters::Robomaster;
 
 using ::testing::Return;
 using ::testing::_;
 
-TEST_F(PnpsolverTest,AbilityTest)
+TEST_P(PnpsolverTest,AbilityTest)
 {
+    ArmorIPPEPnPSolver pnp_solver_test_v1(Robomaster::NormalArmorObjectPointsOpencv,Robomaster::LargeArmorObjectPointsOpencv);
     RunTest();
-}
 
+}
+INSTANTIATE_TEST_SUITE_P(PnpsolverTest, PnpsolverTest, ::testing::Values(
+    new ArmorIPPEPnPSolver(Robomaster::NormalArmorObjectPointsOpencv,Robomaster::LargeArmorObjectPointsOpencv)
+    ));
+
+    // INSTANTIATE__SUITE_P()
+    // 测试套件
+    // 测试类名
+    // 测试类的参数传入->GetParam()
 int main(int argc, char **argv) {
     ::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
