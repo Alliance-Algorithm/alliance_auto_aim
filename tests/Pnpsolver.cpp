64db0f2410f804630897599cfd06872ea1975064
HarryPotter1tech <huangziang_2024@qq.com>
2025-10-10 20:20:58 +0800

    修改测试类，继承自testing::TestWithParam<IPnpSolver*>参数化测试，传入使用接口类，修改命名空间

--- tests/Pnpsolver.cpp
+++ tests/Pnpsolver.cpp
@@ -1,26 +1,25 @@
 #include <gtest/gtest.h>
 #include <interfaces/pnp_solver.hpp>
 #include "mocks/MockArmor2D.hpp"
-#include "v1/pnpsolver/armor_pnp_solver.hpp"
+#include "interfaces/pnp_solver.hpp"
 #include "parameters/rm_parameters.hpp"
 #include "mocks/MockHikCameraProfile.hpp"
 
-using world_exe::v1::pnpsolver::ArmorIPPEPnPSolver;
+using world_exe::interfaces::IPnpSolver;
 using world_exe::tests::mock::MockArmorInImage;
-using world_exe::parameters::Robomaster;
 
-void RunableTest(world_exe::interfaces::IPnpSolver& pnp_solver)
-{
+class PnpsolverTest : public ::testing::TestWithParam<IPnpSolver*> {
+public:
+    PnpsolverTest() : pnp_solver_test(GetParam()) {}
+
+    void RunableTest()
+    {
     auto mockarmor = MockArmorInImage::createMockArmorInImage();
-    pnp_solver.SolvePnp(mockarmor);
-}
-
-class PnpsolverTest : public ::testing::Test {
-protected:
-    ArmorIPPEPnPSolver pnp_solver;
-    PnpsolverTest(): pnp_solver( Robomaster::NormalArmorObjectPointsOpencv,Robomaster::LargeArmorObjectPointsOpencv){}
-
-    void RunTest() {
-        RunableTest(pnp_solver);
+    pnp_solver_test->SolvePnp(mockarmor);
     }
+    void RunTest() {
+        RunableTest();
+    }
+private:
+    IPnpSolver* pnp_solver_test;
 };
